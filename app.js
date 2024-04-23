855970d7aca0323f508c99805f53004465b3b326
Robin Singh <robin17singh@users.noreply.github.com>
2024-04-23 15:05:54 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -18,22 +18,20 @@ app.get('/open-positions', async (req, res) => {
     // Fetch Actian Careers page HTML
     const response = await axios.get('https://www.actian.com/company/careers');
     const html = response.data;
-    console.log(response.data)
     // Load HTML into Cheerio for easy DOM traversal
     const $ = cheerio.load(html);
-    console.log(html)
     // Find the department section
     const accordionDiv = $(`span.category-name:contains(${department})`).parentsUntil(".accordion-item").siblings().find("div.job-name")
       // Check if department parameter is provided
     if (accordionDiv.length===0) {
       return res.status(400).json({ error: 'Department is required!' });
     }
-    const data =  []
+    const job_title =  []
     for (let i=0; i < accordionDiv.length; i++){
-      data.push(accordionDiv.eq(i).text())
+      job_title.push(accordionDiv.eq(i).text())
     }
    
-    return res.json({ department, data });
+    return res.json({ department, job_title });
   } catch (error) {
     console.error('Error:', error);
     return res.status(500).json({ error: 'Internal server error' });
